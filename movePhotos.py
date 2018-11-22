af4b6ccc688a3743429278d47497f035c006abc5
Torfinn Aas <torfinn.aas@gmail.com>
2018-11-22 23:39:02 +0100

    Added file counter

--- movePhotos.py
+++ movePhotos.py
@@ -14,7 +14,9 @@ VERSION='0.0.1'
 EXIF_DATETIME_TAG=36867
 # Date/time format in EXIF data from jpeg files: '2016:03:25 21:29:36'
 DATETIME_PATTERN='%Y:%m:%d %H:%M:%S'
-monthDirName = ['01 Januar', '02 Februar', '03 Mars', '04 April', '05 Mai', '06 Juni', '07 Juli', '08 August', '09 September', '10 Oktober', '11 November', '12 Desember']
+monthDirName = ['01 Januar', '02 Februar', '03 Mars', '04 April', '05 Mai', '06 Juni',
+                '07 Juli', '08 August', '09 September', '10 Oktober', '11 November', '12 Desember']
+filesMoved = 0
 
 
 
@@ -45,8 +47,11 @@ def initial():
 
 
 def traversDirectories(src_dir, dest_dir):
+    global filesMoved
+
     for root, dirs, files in os.walk(src_dir):
         print(f'Directory: {root}:')
+
         for file in files:
             pathname = os.path.join(root, file)
             temp, file_extension = os.path.splitext(file)
@@ -64,12 +69,15 @@ def traversDirectories(src_dir, dest_dir):
                 # Move/copy the file
                 newpathname = dest_dir + '/' + str(year) + '/' + monthDirName[month-1] + '/' + file;
                 print('Copies from: ', pathname, ' ==> to : ', newpathname)
-                shutil.copy2(pathname, newpathname)
+                #shutil.copy2(pathname, newpathname)
+                shutil.move(pathname, newpathname)
+                filesMoved += 1
 
 
 
 if __name__ == '__main__':
     curr_dir, new_dir = initial()
     traversDirectories(curr_dir, new_dir)
+    print(f'Files moved: {filesMoved}')
 else:
     print('This module can only be executed standalone')
